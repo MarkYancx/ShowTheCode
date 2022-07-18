fcf55265b32c4da369609feccf98b91f4754a58d
MarkYan <1036911602@qq.com>
2022-07-18 16:11:44 +0800

    symbol

--- src/JavaScript/bind.js
+++ src/JavaScript/bind.js
@@ -1,4 +1,9 @@
 // newFn = fn.bind(obj)
-Function.prototype._bind = function (obj) {
-    // this
+Function.prototype._bind = function (obj, ...arguments1) {
+    // this值指的是fn
+    const _this = this;
+    
+    return function (...arguments2) {
+        return _this.apply(target, arguments1.concat(arguments2));
+    }
 }
